da6837f11adbf830f238a914e1bb0ce0a214e1ee
cinyee1214 <xinyizhao1214@gmail.com>
2020-05-02 13:41:24 -0400

    connection

--- Project/config/mongoConnection.js
+++ Project/config/mongoConnection.js
@@ -12,9 +12,9 @@ const mongoConfig = settings.mongoConfig;
 let _connection = undefined;
 let _db = undefined;
 
-module.exports = async () => {
+module.exports = async() => {
     if (!_connection) {
-        _connection = await MongoClient.connect(mongoConfig.serverUrl, {useNewUrlParser: true});
+        _connection = await MongoClient.connect(mongoConfig.serverUrl, { useNewUrlParser: true, useUnifiedTopology: true });
         _db = await _connection.db(mongoConfig.database);
     }
 
