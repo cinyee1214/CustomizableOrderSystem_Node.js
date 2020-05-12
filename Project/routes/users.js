21faeb0aaeaf5b5ea9b59588a5af1bada2526f07
ZimingZhang957 <55264433+ZimingZhang957@users.noreply.github.com>
2020-05-12 12:19:04 -0400

    Update users.js

--- Project/routes/users.js
+++ Project/routes/users.js
@@ -111,6 +111,12 @@ router.patch('/', async(req, res) => {
         res.status(401).json({ error: error });
         return;
     }
+    
+    if (password.length < 8) {
+        const error = "401 : the password length cannot be less than 8!";
+        res.status(401).json({ error: error });
+        return;
+    }
 
     if (password && cpassword && password !== cpassword) {
         const error = "401 : The new password and the confirmed password are not the same!";
